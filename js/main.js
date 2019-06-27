ef61f632e981b465af4966ffb5fd2ae6843f9136
commaeng <commaeng09@gmail.com>
2019-06-27 22:15:41 +0900

    slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-$("")
+$(".navi > li").hover(function(){
+	// $(this).find(".subs").stop().slideDown(500);
+	$(this).find(".subs").stop().fadeIn(1000);
+}, function(){
+	// $(this).find(".subs").stop().slideUp(500);
+	$(this).find(".subs").stop().fadeOut(1000);
+});
